c3aa6a81d72edef91214cb3523d86adc069fb423
KokosenPLS <43403941+KokosenPLS@users.noreply.github.com>
2021-02-10 14:33:08 +0100

    Update reg.php

--- kobling/reg.php
+++ kobling/reg.php
@@ -10,24 +10,23 @@
 
       require 'kob.php';
 
-      $navn = $_POST['navn'];
-      $tlf = $_POST['telefon'];
+      $fnavn = $_POST['fnavn'];
+      $enavn = $_POST['enavn'];
       $dato = $_POST['date'];
       $epost = $_POST['epost'];
       $passord = $_POST['passord'];
       $bpassord = $_POST['bpassord'];
 
 
-      if (empty($navn) || empty($tlf) || empty($dato) || empty($epost) || empty($passord) || empty($bpassord)){
+      if (empty($fnavn) || empty($enavn) || empty($dato) || empty($epost) || empty($passord) || empty($bpassord)){
         header("Location: ../innlogging.php?error=tommefelt");
         exit();
       } else if ($passord != $bpassord) {
         header("Location: ../innlogging.php?error=passord_samsvarer_ikke");
       } else {
-            $sql = "INSERT INTO person (navn, tlf, fdato, epost, profilbilde) VALUES ('$navn', $tlf, '$dato', '$epost', '')";
+            $sql = "CALL LeggTilBruker('$fnavn', '$enavn', '$dato', '$epost', '$bpassord')";
             if ($conn->query($sql) === TRUE) {
-              $sql1 = "INSERT INTO innlogging (epost, passord) VALUES ('$epost', '$passord')";
-                if ($conn->query($sql1) === TRUE) {
+                if ($conn->query($sql) === TRUE) {
                   header("Location: ../index.php?registrering_vellykket");
                 } else {
                   echo "Error: " . $sql . "<br>" . $conn->error;
